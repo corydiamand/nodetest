851c4038183ad35ae1cee689dde8e703d9fa778d
Cory Diamand <corydiamand@gmail.com>
2012-08-27 17:58:36 -0400

    Adding in socket.io

--- app.js
+++ app.js
@@ -7,16 +7,21 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path');
+ 
+  
+  require('events').EventEmitter;
 
 var app = express();
+//app.listen(3000);
 //var $ = require('jQuery');
+io = require('socket.io').listen(app);
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
+//  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -29,6 +34,17 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+console.log(app.get("connections"))
